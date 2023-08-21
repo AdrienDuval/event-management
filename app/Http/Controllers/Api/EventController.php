87c175887b22c25df3b7ff53df2ee234ecd0c54e
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-08-22 00:38:56 +0530

    Eloquent API resources
    Eloquent API Resources in Laravel allow you to transform your Eloquent models and their relationships into a format suitable for your API responses. It gives you fine-grained control over how your data is presented, making it easy to customize the structure, include or exclude fields, and handle relationships. Here's a brief explanation with an example:
    
    1. **What are Eloquent API Resources?**
       - Eloquent API Resources are classes that define how your Eloquent models should be represented in API responses.
       - They allow you to structure and format the data in a way that best suits your API consumers.
    
    2. **Why Use Eloquent API Resources?**
       - Separation of Concerns: API Resources separate the data presentation logic from your models, promoting clean and maintainable code.
       - Customization: You can control the data fields, relationships, and additional metadata included in the API responses.
       - Consistency: API Resources ensure a consistent data format across different API endpoints.
    
    3. **Creating an Eloquent API Resource:**
       - Use the `php artisan make:resource` Artisan command to create a new API resource.
    
       ```bash
       php artisan make:resource PostResource
       ```
    
    4. **Defining the API Resource:**
       - Open the newly created resource class in the `app/Http/Resources` directory.
       - Define the transformation logic in the `toArray` method.
    
       ```php
       namespace App\Http\Resources;
    
       use Illuminate\Http\Resources\Json\JsonResource;
    
       class PostResource extends JsonResource
       {
           public function toArray($request)
           {
               return [
                   'id' => $this->id,
                   'title' => $this->title,
                   'body' => $this->body,
                   'created_at' => $this->created_at->format('Y-m-d H:i:s'),
                   // Additional custom fields or relationships
               ];
           }
       }
       ```
    
    5. **Using the API Resource:**
       - In your controller, use the API resource to transform the model instance when returning responses.
    
       ```php
       use App\Http\Resources\PostResource;
    
       public function show($id)
       {
           $post = Post::findOrFail($id);
           return new PostResource($post);
       }
       ```
    
    6. **Relationships in API Resources:**
       - You can handle model relationships within API resources by defining methods for related data.
    
       ```php
       public function toArray($request)
       {
           return [
               'id' => $this->id,
               'title' => $this->title,
               'comments' => CommentResource::collection($this->comments),
           ];
       }
       ```
    
    7. **Collection Resources:**
       - For returning collections of resources, you can create collection resources using the `php artisan make:resource` command with the `--collection` flag.
    
       ```bash
       php artisan make:resource PostCollection
       ```
    
    8. **Using Collection Resources:**
       - In your controller, use the collection resource to transform a collection of model instances.
    
       ```php
       use App\Http\Resources\PostCollection;
    
       public function index()
       {
           $posts = Post::all();
           return new PostCollection($posts);
       }
       ```
    
    Eloquent API Resources provide a flexible way to format and structure your API responses while keeping your code organized. By using these resources, you can ensure that your API responses are consistent, efficient, and tailored to the needs of your API consumers.

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use Illuminate\Http\Request;
 use App\Models\Event;
 
@@ -13,7 +14,7 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        return EventResource::collection(Event::with('user')->get());
     }
 
     /**
@@ -34,7 +35,7 @@ class EventController extends Controller
 
         ]);
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
@@ -42,7 +43,8 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
@@ -57,7 +59,7 @@ class EventController extends Controller
             'end_time' => 'sometimes|date|after:start_time'
         ]));
 
-        return $event;
+        return new EventResource($event);
     }
 
     /**
