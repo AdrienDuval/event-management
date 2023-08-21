5c26677c56d0292dd4439c32fd3cdeb561fff747
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-08-22 00:00:57 +0530

    Adding update and delete functionallity

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -24,7 +24,7 @@ class EventController extends Controller
 
 
         $event = Event::create([
-            ... $request->validate([
+            ...$request->validate([
                 'name' => 'required|string|max:255',
                 'description' => 'nullable|string',
                 'start_time' => 'required|date',
@@ -48,16 +48,24 @@ class EventController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+        $event->update($request->validate([
+            'name' => 'sometimes|string|max:255',
+            'description' => 'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]));
+
+        return $event;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+        return response(status: 204);
     }
 }
